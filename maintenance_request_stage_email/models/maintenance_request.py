4a1cc244442ea7681e1f7414a172615856954207
David Alonso // Solvos <david.alonso@solvos.es>
2021-07-06 14:22:26 +0200

    [FIX] maintenance_request_stage_email: several issues
    Fixes:
    - wrong company selection con request create email
    - view protection and templates improvements
    - prevent sending mail for request from done to done stage

--- maintenance_request_stage_email/models/maintenance_request.py
+++ maintenance_request_stage_email/models/maintenance_request.py
@@ -6,22 +6,36 @@ from odoo import models, api
 class MaintenanceRequest(models.Model):
     _inherit = "maintenance.request"
 
-    @api.model 
+    @api.model
     def create(self, vals):
         request = super().create(vals)
-        if self.env.user.company_id.maintenance_request_open_user_ids:
-            request_users = self.env.user.company_id.maintenance_request_open_user_ids
+        if request.company_id.maintenance_request_open_user_ids:
+            request_users = request.company_id.maintenance_request_open_user_ids
             for user in request_users.filtered(lambda x: x.email):
                 values = {"user_notify_id": user.id, "user_notify_email": user.email}
                 template = self.env.ref('maintenance_request_stage_email.maintenance_request_created_email_template')
                 template.with_context(values).send_mail(request.id, email_values=None, force_send=True)
         return request
 
-    @api.constrains('stage_id')
-    def _constrains_stage_id(self):
-        if self.stage_id.done and self.equipment_id.users_notify_closed_ids:
-            request_users = self.equipment_id.users_notify_closed_ids
-            for user in request_users.filtered(lambda x: x.email):
-                values = {"user_notify_id": user.id, "user_notify_email": user.email}
-                template = self.env.ref('maintenance_request_stage_email.maintenance_request_closed_email_template')
-                template.with_context(values).send_mail(self.id, email_values=None, force_send=True)
+    @api.multi
+    def write(self, values):
+        requests = self.filtered(
+            lambda r: not r.stage_id.done and r.equipment_id.users_notify_closed_ids
+        )
+        res = super().write(values)
+        if requests and values.get("stage_id", False):
+            new_stage_id = self.env["maintenance.stage"].browse(
+                values.get("stage_id")
+            )
+            if new_stage_id.done:
+                template = self.env.ref(
+                    "maintenance_request_stage_email.maintenance_request_closed_email_template"
+                )
+                for record in requests:
+                    for user in record.equipment_id.users_notify_closed_ids:
+                        tmpl_vals = {"user_notify_id": user.id, "user_notify_email": user.email}
+                        template.with_context(tmpl_vals).send_mail(
+                            record.id, email_values=None, force_send=True
+                        )
+
+        return res
