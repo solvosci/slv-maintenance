0679d77ea3e0f758895c946b071fe4d791b192fc
Iria Alonso <iria.alonso@solvos.es>
2026-01-22 14:03:05 +0100

    [MIG] product_maintenance_equipment_link: Migration to 17.0

--- product_maintenance_equipment_link/models/maintenance_equipment.py
+++ product_maintenance_equipment_link/models/maintenance_equipment.py
@@ -2,6 +2,8 @@
 # License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
 
 from odoo import models, fields, _
+from odoo.fields import Command
+
 
 class MaintenanceEquipment(models.Model):
     _inherit = 'maintenance.equipment'
@@ -12,17 +14,17 @@ class MaintenanceEquipment(models.Model):
     def compute_product_count(self):
         for record in self:
             record.count_product_ids = len(record.product_ids)
-    
+
     def action_view_products(self):
         action = {
             'name': _('Products'),
             'res_model': 'product.product',
             'type': 'ir.actions.act_window',
             'context':{
-                'default_maintenance_equipment_ids': [(4, self.ids)]
+                'default_maintenance_equipment_ids': Command.link(self.ids)
             }
         }
-        
+
         if self.count_product_ids == 1:
             action['view_mode'] = 'form'
             action['res_id'] = self.product_ids.id
--- product_maintenance_equipment_link/models/product.py
+++ product_maintenance_equipment_link/models/product.py
@@ -3,6 +3,7 @@
 
 from odoo import models, fields
 
+
 class ProductProduct(models.Model):
     _inherit = 'product.product'
 
